4af3300f9e09f02fe4b6777fd243e47ef3ddca8c
BADR <benabboudbadr@gmail.com>
2023-03-19 14:44:00 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+
 /**
-*main - entry point
-*return : always 0 (success)
+ *main - Entry point
+ *
+ *Description: A C program that print function with puts
+ *
+ *Return: Always 0 (Success)
 */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
