259f6b534b5f97b16f360ec0b8e149dc31664b3b
BADR <benabboudbadr@gmail.com>
2023-03-19 15:03:02 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,12 +5,12 @@
  *
  *Description: A C program that print function with printf
  *
- *Return: Always 0 (Success)
+*Return: Always 0 (Success)
 */
 
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
 
